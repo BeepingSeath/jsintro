758d7d75c449ca216578b9d5361e265e7c36174f
= <Beeping.Seath.DKS@gmail.com>
2023-09-05 14:26:53 +0200

    ✨ Variabler

---
+++ variabler.js
@@ -0,0 +1,16 @@
+function CL (val) {
+    console.log(val)
+}
+
+let name = 'Seth';
+CL(typeof(name));
+const greeting = `Hej ${name}`;
+CL(greeting);
+let age = 19;
+CL(typeof(age));
+let x = Number.MAX_VALUE;
+CL(name + x);
+let y = x * 2 +1;
+CL(typeof(y));
+CL(isNaN(y));
+CL(y);
